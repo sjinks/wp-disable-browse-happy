ff5367079d4520d3690921120f8c55f39d8aa7d7
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-06-03 04:27:37 +0300

    Bump version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WW Disable browse-happy
  * Plugin URI: https://github.com/sjinks/wp-disable-browse-happy
  * Description: Disable calls to the BrowseHappy API (ie, for privacy reasons)
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Volodymyr Kolesnykov
  * License: MIT
  */
